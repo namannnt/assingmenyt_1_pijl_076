7af6964b1bb39ccfe1bdc053db0e618f57333c36
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:27:16 +0530

    Creating divide method

--- Calculator.java
+++ Calculator.java
@@ -10,8 +10,18 @@ public class Calculator {
   public double subtract(double a, double b) { 
         return a - b; 
     }
+
   //Creating method for multiplication
   public double multiply(double a, double b) { 
         return a * b; 
+    }
+  //Creating method for division
+  public double divide(double a, double b) { 
+        if (b != 0) {
+            return a / b ;
+        }
+        else {
+            return Double.NaN ;
+        }
     }
 }
