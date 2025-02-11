97cb3e4c2f76799bf73cc5b2f8e6352d606619a2
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:11:52 +0530

    Adding the rest switch cases Adding the rest switch cases of mean, sum of array, variance and standard deviation

--- UserInput.java
+++ UserInput.java
@@ -35,6 +35,7 @@ public class UserInput{
         double b = scan.nextDouble();
         System.out.println("Result: " + calculator.multiply(a, b));
       }
+
       //Division  
       case 4 -> {
         System.out.println("Enter two numbers:");
@@ -42,12 +43,57 @@ public class UserInput{
         double b = scan.nextDouble();
         System.out.println("Result: " + calculator.divide(a, b));
       }
+
       //Fibonacci Series
       case 5 -> {
         System.out.println("Enter a number:");
         int n = scan.nextInt();
         System.out.println("Fibonacci: " + calculator.fibonacci(n));
       }
+
+      case 6 -> {
+        System.out.println("Enter array size:");
+        int size = scan.nextInt();
+        double[] arr = new double[size];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < size; i++) {
+          arr[i] = scan.nextDouble();
+        }
+        System.out.println("Sum: " + calculator.sumArray(arr));
+      }
+            
+      case 7 -> {
+        System.out.println("Enter array size:");
+        int size = scan.nextInt();
+        double[] arr = new double[size];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < size; i++) {
+          arr[i] = scan.nextDouble();
+        }
+        System.out.println("Mean: " + calculator.mean(arr));
+      }
+            
+      case 8 -> {
+        System.out.println("Enter array size:");
+        int size = scan.nextInt();
+        double[] arr = new double[size];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < size; i++) {
+          arr[i] = scan.nextDouble();
+        }
+        System.out.println("Variance: " + calculator.variance(arr));
+      }
+            
+      case 9 -> {
+        System.out.println("Enter array size:");
+        int size = scan.nextInt();
+        double[] arr = new double[size];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < size; i++) {
+          arr[i] = scan.nextDouble();
+        }
+        System.out.println("Standard Deviation: " + calculator.standardDeviation(arr));
+      }  
 
     }
   }
