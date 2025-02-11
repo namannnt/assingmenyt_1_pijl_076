39db9c16acbe0484dec4a07c5068ce1aca7d714a
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:08:27 +0530

    Creating method handleOperations

--- UserInput.java
+++ UserInput.java
@@ -1,10 +1,12 @@
-//UserInput.java
-import java.util.* ;
 public class UserInput{
   private Scanner scan;
-
+  private final Scanner scan;
+  
   public UserInput(Scanner scan) {
         this.scan = scan;
     }
-  
+  //Creating method handleOperations
+  public void handleOperation(int choice, Calculator calculator) {
+
+  }
 }
