b63d90de5d1998213017f5f081d9a8728eea4fb3
Naman Mittal <mittalnaman2005@gmail.com>
2025-02-11 11:28:28 +0530

    Creating method sumArray

--- Calculator.java
+++ Calculator.java
@@ -25,6 +25,7 @@ public class Calculator {
             return Double.NaN ;
         }
     }
+
   //Creating fibonacci method for fibonacci sequence
   public int fibonacci(int n) {
         if (n <= 1) {
@@ -33,5 +34,13 @@ public class Calculator {
         else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }   
+    }
+  //Creating method for the sum of array
+  public double sumArray(double[] arr) {
+        double sum = 0;
+        for (double num : arr) {
+            sum += num;
+        }
+        return sum;
     }
 }
